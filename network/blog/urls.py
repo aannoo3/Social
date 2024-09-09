83f0fc08e1309ad4fcfe53b334e2b2f5bd628c7b
Anwar Khan <aannookhan3@gmail.com>
2024-09-09 15:35:32 +0500

    CRUD completed

--- network/blog/urls.py
+++ network/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import PostListView, PostDetailView, PostUpdateView, PostDeleteView, PostCreateView
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create new post
+    path('', PostListView.as_view(), name='post-list'),  # List of posts
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # Single post detail
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # Update post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete post
 ]
