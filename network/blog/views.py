35b050d14f209c3d7991f5f7dd205751756cf5f7
Anwar Khan <aannookhan3@gmail.com>
2024-09-09 14:43:49 +0500

    Updated views and forms for blog and users, added .gitignore

--- network/blog/views.py
+++ network/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from . models import Post
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 # Create your views here.
 
 def home(request):
     posts = Post.objects.all()
     return render(request, 'blog/base.html', {'posts': posts})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/base.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
