f0850fea91bdfeb56a6c33e73701ce4b3ad87ab1
Anwar Khan <aannookhan3@gmail.com>
2024-09-10 14:07:31 +0500

    Finalize password reset functionality

--- network/network/settings.py
+++ network/network/settings.py
@@ -136,3 +136,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 LOGIN_REDIRECT_URL = 'post-list'
 LOGOUT_REDIRECT_URL = 'login'
+
+
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  # Ensure port is an integer
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'  # Convert 'True' string to boolean
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+
+
+# Enable logging to get detailed output of email sending process
+import logging
+logging.basicConfig(level=logging.DEBUG)
